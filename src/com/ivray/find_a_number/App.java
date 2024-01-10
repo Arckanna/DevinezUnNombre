c1b77630b059203d12634e12dfea5803e08bd6f8
Arckanna <valerie.lecoeur75@gmail.com>
2024-01-10 12:17:17 +0100

    First commit
    Create declarative part and the first draft of the application

--- src/com/ivray/find_a_number/App.java
+++ src/com/ivray/find_a_number/App.java
@@ -1,10 +1,31 @@
 package com.ivray.find_a_number;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Declarative part
+		int nbToFind = 0;
+		Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
+		int nbEnter =0;
+		int nbTrial = 0;
+		
+		//Processing part	
+		System.out.println("Welcome ! Please enter a number between 1 and 100");
+		nbToFind = random.nextInt(100) + 1;
+		nbEnter = scanner.nextInt();
+		
+		if (nbEnter > nbToFind) {
+			System.out.println("Smaller");
+		}else if (nbEnter < nbToFind) {
+			System.out.println("Bigger");
+		}else {
+			System.out.println("GG! You win in " + nbTrial + " trial.");
+		}
+		
 	}
 
 }
