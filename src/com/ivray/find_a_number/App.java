94b6709add6e3759312c9b793ef062094d158fd0
Arckanna <valerie.lecoeur75@gmail.com>
2024-01-11 18:53:56 +0100

    Catch error
    Add a try catch for catch the user error

--- src/com/ivray/find_a_number/App.java
+++ src/com/ivray/find_a_number/App.java
@@ -1,36 +1,59 @@
 package com.ivray.find_a_number;
 
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+
+	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// Declarative part
 		int nbToFind = 0;
 		Random random = new Random();
 		Scanner scanner = new Scanner(System.in);
-		int nbEnter =0;
+		int nbEnter = 0;
 		int nbTrial = 0;
-		
-		//Processing part	
-		System.out.println("Welcome ! Please enter a number between 1 and 100");
+
+		// Processing part
+		System.out.println("Welcome ! ");
 		nbToFind = random.nextInt(100) + 1;
-		
+
 		while (nbEnter != nbToFind) {
 			nbTrial++;
-			nbEnter = scanner.nextInt();
-			
+			nbEnter = askNumber("Enter a number between 1 and 100", 1, 100);
+
 			if (nbEnter > nbToFind) {
 				System.out.println("Smaller");
-			}else if (nbEnter < nbToFind) {
+			} else if (nbEnter < nbToFind) {
 				System.out.println("Bigger");
-			}else {
+			} else {
 				System.out.println("GG! You win in " + nbTrial + " trial.");
 			}
 		}
-		
-		
+		scanner.close();
+
+	}
+
+	private static int askNumber(String message, int min, int max) {
+		int nbValue = min - 1;
+
+		do {
+			System.out.println(message);
+			try {
+				nbValue = scanner.nextInt();
+			} catch (NoSuchElementException e) {
+				System.out.println("Please enter a number");
+				scanner.next();
+			} catch (IllegalStateException e) {
+				System.out.println("Please enter a number");
+				System.exit(-1);
+			}
+
+		} while (nbValue < min || nbValue > max);
+		return nbValue;
+
 	}
 
 }
